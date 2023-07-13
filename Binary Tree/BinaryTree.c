eea1f06ecff65981694307e702846dbe471016e4
parthpetkar <callofdutyparth2004@gmail.com>
2023-07-13 11:13:47 +0530

    level order tranversal added

--- Binary Tree/BinaryTree.c
+++ Binary Tree/BinaryTree.c
@@ -28,16 +28,96 @@ struct node *create()
         scanf("%d", &data);
         temp->data = data;
 
-        printf("Enter left child of the tree:");
+        printf("Enter left child of the tree: ");
         temp->left = create();
 
-        printf("Enter right child of the tree:");
+        printf("Enter right child of the tree: ");
         temp->right = create();
     }
     else
     {
         printf("Invalid choice!\n");
         return NULL;
+    }
+
+    return temp;
+}
+
+struct QNode
+{
+    struct node *key;
+    struct QNode *next;
+};
+
+struct Queue
+{
+    struct QNode *front, *rear;
+};
+
+struct QNode *newNode(struct node *k)
+{
+    struct QNode *temp = (struct QNode *)malloc(sizeof(struct QNode));
+    temp->key = k;
+    temp->next = NULL;
+    return temp;
+}
+
+struct Queue *createQueue()
+{
+    struct Queue *q = (struct Queue *)malloc(sizeof(struct Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+void enQueue(struct Queue *q, struct node *k)
+{
+    struct QNode *temp = newNode(k);
+
+    if (q->rear == NULL)
+    {
+        q->front = q->rear = temp;
+        return;
+    }
+
+    q->rear->next = temp;
+    q->rear = temp;
+}
+
+struct node *deQueue(struct Queue *q)
+{
+    if (q->front == NULL)
+        return NULL;
+
+    struct QNode *temp = q->front;
+    struct node *node = temp->key;
+
+    q->front = q->front->next;
+
+    if (q->front == NULL)
+        q->rear = NULL;
+
+    free(temp);
+    return node;
+}
+
+void levelOrderTraversal(struct node *root)
+{
+    if (root == NULL)
+        return;
+
+    struct Queue *q = createQueue();
+    enQueue(q, root);
+
+    while (q->front != NULL)
+    {
+        struct node *node = deQueue(q);
+        printf("%d ", node->data);
+
+        if (node->left != NULL)
+            enQueue(q, node->left);
+
+        if (node->right != NULL)
+            enQueue(q, node->right);
     }
 }
 
@@ -50,7 +130,8 @@ int main()
     {
         printf("\n----- Menu -----\n");
         printf("1. Create Binary Tree\n");
-        printf("2. Exit\n");
+        printf("2. Perform Level Order Traversal\n");
+        printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -60,13 +141,18 @@ int main()
             root = create();
             break;
         case 2:
+            printf("Level Order Traversal: ");
+            levelOrderTraversal(root);
+            printf("\n");
+            break;
+        case 3:
             printf("Exiting the program.\n");
             break;
         default:
             printf("Invalid choice. Please try again.\n");
             break;
         }
-    } while (choice != 2);
+    } while (choice != 3);
 
     return 0;
 }
